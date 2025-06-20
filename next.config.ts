fd96d96f65a3d7cd194553db40eb36597645db3f
Mega-ValeSup <suporte@megavalecard.com.br>
2025-06-20 17:15:34 -0300

    config do next.config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  //output: 'export', // Habilita o modo static export
+  trailingSlash: true, // Adiciona barra no final das rotas (opcional, recomendado para static hosting)
+  images: {
+    unoptimized: true, // Necessário para exportação, pois o Next otimiza imagens apenas em servidor
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
